8bd85009f0c138b5c71f6b620675dbd7acfc2bb1
Ivan Gromov <ivan.gromov@redsolution.ru>
2010-11-19 18:21:57 +0500

    Fixed key error. Undocumented yet

--- feedback/views.py
+++ feedback/views.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from feedback.utils import get_feedback_form
 
 
 def show_ajax_response(request, key='default'):
-    FormClass = get_feedback_form(key)
     if request.method == 'POST':
         FormClass = get_feedback_form(request.POST.get('form_settings_key', key))
         form = FormClass(request.POST)
@@ -14,7 +14,8 @@ def show_ajax_response(request, key='default'):
             form.mail()
             return render_to_response('feedback/thankyou.html', {'form': form},
                 context_instance=RequestContext(request))
+        else:
+            return render_to_response('feedback/feedback.html', {'form': form},
+                context_instance=RequestContext(request))
     else:
-        form = FormClass()
-    return render_to_response('feedback/feedback.html', {'form': form},
-        context_instance=RequestContext(request))
+        return HttpResponseRedirect('/')

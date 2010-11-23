b70b377257fa1f05afb682f1f08ee755c5e6816b
Ivan Gromov <ivan.gromov@redsolution.ru>
2010-11-23 19:35:11 +0500

    Changed javascript to work with several forms

--- feedback/media/feedback/js/feedback.js
+++ feedback/media/feedback/js/feedback.js
@@ -1,18 +1,20 @@
 ;
-$(document).ready(function(){
-    $('#id_feedback_form').live('submit', function(){
+feedback = {
+    submit: function(form){
         var data = {};
-        var feedback_url = $(this).attr('action');
-        $(this).find(':input').each(function(){
+        var feedback_url = $(form).attr('action');
+        $(form).find(':input').each(function(){
             var key = $(this).attr('name');
             var value = $(this).attr('value');
             data[key] = value;
         });
-        $.post(feedback_url, data, load_feedback);
+        
+        console.log($(form).find(':input'));
+        console.log(data);
+        
+        $.post(feedback_url, data, function(data, textStatus){
+            $(form).replaceWith(data).show();
+        });
         return false;
-    });
-});
-
-function load_feedback(data, textStatus){
-    $('#id_feedback_form').replaceWith(data).show();
-}
+    }
+};

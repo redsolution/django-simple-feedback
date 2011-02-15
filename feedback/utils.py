db192e8266388580dedf16654d6c088062b6fe7e
Ivan Gromov <ivan.gromov@redsolution.ru>
2011-02-15 18:53:42 +0500

    Removed old function, fixed function name

--- feedback/utils.py
+++ feedback/utils.py
@@ -5,16 +5,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django.conf import settings
 from django.utils.translation import string_concat
 
-
-# Not used in Django >= 1.2
-def email_backend(recipient_list, message, subject='Feedback'):
-    subject = settings.EMAIL_SUBJECT_PREFIX + subject
-    if type(recipient_list) is not list:
-        recipient_list = [recipient_list, ]
-
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
-              recipient_list, fail_silently=False)
-
 def import_item(path, error_text):
     """Imports a model by given string. In error case raises ImpoprelyConfigured"""
     i = path.rfind('.')
@@ -30,8 +20,8 @@ def get_feedback_form(key):
         raise ImproperlyConfigured('Form %s not registered in FEEDBACK_FORMS' % key)
     return import_item(FEEDBACK_FORMS[key], 'can not import feedback form')
 
-def mail_managers(subject, message, fail_silently):
-    '''Repalcement for standars Django function.
+def mail_admins(subject, message, fail_silently):
+    '''Repalcement for standard Django function.
     It uses ``DEFAULT_FROM_EMAIL`` setting instead ``SERVR_FROM_EMAIL``
     '''
     if not settings.ADMINS:

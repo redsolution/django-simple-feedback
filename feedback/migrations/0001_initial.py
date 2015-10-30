853ab1f3b4adb42a441248829d15514254edf510
konstantin.gabbasov <konstantin.gabbasov@redsolution.ru>
2015-10-30 16:37:16 +0500

    Remove form choices from initial migration.

--- feedback/migrations/0001_initial.py
+++ feedback/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200, null=True, verbose_name='List title')),
-                ('form', models.CharField(unique=True, max_length=100, verbose_name='Feedback form', choices=[(b'default', b'Default'), (b'email_us', '\u0424\u043e\u0440\u043c\u0430 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u044f \u0441 \u0441\u0430\u0439\u0442\u0430')])),
+                ('form', models.CharField(unique=True, max_length=100, verbose_name='Feedback form')),
                 ('default_from', models.EmailField(max_length=200, null=True, verbose_name='Default sender email', blank=True)),
                 ('emails', models.ManyToManyField(related_name='forms', verbose_name='List of addresses', to='feedback.FeedbackEmail', blank=True)),
             ],
